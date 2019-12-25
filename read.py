7277953b5e6a8b185f43916a74c7551966b0ef72
David Wu <yefan.wu@outlook.com>
2019-12-25 15:26:11 +0800

    read and write basic

---
+++ read.py
@@ -0,0 +1,6 @@
+from openpyxl import load_workbook
+
+wb = load_workbook(filename='readexcel.xlsx')
+# sheet_ranges = wb['range names']
+# print(sheet_ranges['A2'].value)
+# print(type(sheet_ranges))
